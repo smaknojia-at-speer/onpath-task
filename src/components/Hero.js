644f4812b966da02ce0b99d18bee2d8ff30c2209
Saif Maknojia <66653328+SaifMaknojia@users.noreply.github.com>
2022-10-06 00:09:06 -0400

    fix images alignment

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,21 +20,27 @@ const Hero = () => {
           <CustomButton bgcolour="primary" content="Get Started" />
         </div>
         <div className="d-flex hero-phones-container justify-content-center pt-5 mt-4">
-          <img
-            className="hero-phone-images mt-5 mt-md-0"
-            src={iphoneLeft}
-            alt="Hero-images"
-          />
-          <img
-            className="hero-phone-images"
-            src={iphoneCenter}
-            alt="Hero-iamges"
-          />
-          <img
-            className="hero-phone-images mt-5 mt-md-0"
-            src={iphoneRight}
-            alt="Hero-Images"
-          />
+          <div>
+            <img
+              className="hero-phone-images mt-5 mt-md-0"
+              src={iphoneLeft}
+              alt="Hero-images"
+            />
+          </div>
+          <div className="mx-0 mx-md-5">
+            <img
+              className="hero-phone-images px-5 px-md-0"
+              src={iphoneCenter}
+              alt="Hero-iamges"
+            />
+          </div>
+          <div>
+            <img
+              className="hero-phone-images mt-5 mt-md-0"
+              src={iphoneRight}
+              alt="Hero-Images"
+            />
+          </div>
         </div>
       </div>
     </>
